4958db520b816ddc6ff0594a500deb845ec389ff
sp00m <christophe.maillard@rocketmail.com>
2017-12-07 19:03:50 +0000

    [1.3.0] Refactoring

--- test/warnings.js
+++ test/warnings.js
@@ -20,86 +20,56 @@ describe("gulp-watch-sass", () => {
     clean()
   })
 
-  it("should warn when @import is duplicated", () => {
+  const testWarning = () => {
+    new ImportTree("**/*.scss", { cwd, warn: console.warn }).build()
+    console.warn.called.should.be.true()
+  }
 
+  it("should warn when @import is duplicated", () => {
     create("a.scss", ["@import 'b.scss';", "@import 'b.scss';"].join("\n"))
     create("b.scss", "div { margin: 0; }")
-
-    new ImportTree("**/*.scss", { cwd, warn: console.warn }).build()
-
-    console.warn.called.should.be.true()
-
+    testWarning()
   })
 
   it("should warn when @import is not found", () => {
-
     create("a.scss", "@import 'b.scss';")
-
-    new ImportTree("**/*.scss", { cwd, warn: console.warn }).build()
-
-    console.warn.called.should.be.true()
-
+    testWarning()
   })
 
   it("should warn when @import is ambiguous (SASS vs partial with extension)", () => {
-
     create("a.scss", "@import 'b.scss';")
     create("b.scss", "div { margin: 0; }")
     create("_b.scss", "div { margin: 0; }")
-
-    new ImportTree("**/*.scss", { cwd, warn: console.warn }).build()
-
-    console.warn.called.should.be.true()
-
+    testWarning()
   })
 
   it("should warn when @import is ambiguous (SASS vs partial without extension)", () => {
-
     create("a.scss", "@import 'b';")
     create("b.scss", "div { margin: 0; }")
     create("_b.scss", "div { margin: 0; }")
-
-    new ImportTree("**/*.scss", { cwd, warn: console.warn }).build()
-
-    console.warn.called.should.be.true()
-
+    testWarning()
   })
 
   it("should warn when @import is ambiguous (CSS vs partial without extension)", () => {
-
     create("a.scss", "@import 'b';")
     create("b.css", "div { margin: 0; }")
     create("_b.scss", "div { margin: 0; }")
-
-    new ImportTree("**/*.scss", { cwd, warn: console.warn }).build()
-
-    console.warn.called.should.be.true()
-
+    testWarning()
   })
 
   it("should warn when @import is ambiguous (CSS vs SASS without extension)", () => {
-
     create("a.scss", "@import 'b';")
     create("b.css", "div { margin: 0; }")
     create("b.scss", "div { margin: 0; }")
-
-    new ImportTree("**/*.scss", { cwd, warn: console.warn }).build()
-
-    console.warn.called.should.be.true()
-
+    testWarning()
   })
 
   it("should warn when @import is ambiguous (CSS vs SASS vs partial without extension)", () => {
-
     create("a.scss", "@import 'b';")
     create("b.css", "div { margin: 0; }")
     create("b.scss", "div { margin: 0; }")
     create("_b.scss", "div { margin: 0; }")
-
-    new ImportTree("**/*.scss", { cwd, warn: console.warn }).build()
-
-    console.warn.called.should.be.true()
-
+    testWarning()
   })
 
 })
